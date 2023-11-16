ad2fc44d4bc6969b2e9a631666225c087aa94e9e
jerh011 <jeelrohe@gmail.com>
2023-11-15 23:51:07 -0700

    Agregar archivos de proyecto.

---
+++ Pilas_Jerh/Pila.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilas_Jerh
+{
+    public class Pila
+    {
+        private int _cima;
+        private int _longitudPila = 5;
+        private int[] _listaPila;
+
+        public Pila()
+        {
+            _cima = -1;
+            _listaPila = new int[_longitudPila];
+        }
+        public bool PilaVacia()
+        {
+            return _cima == -1? true:false;
+        }
+        public bool PilaLlena()
+        {
+            if (_cima == (_longitudPila - 1))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int LongitudPila()
+        {
+            return _longitudPila;
+        }
+        public int CantidadElemento()
+        {
+            return _cima + 1;
+        }
+        public int ElementoCima()
+        {
+            if (PilaVacia())
+            {
+                return _cima;
+            }
+            else
+            {
+                return _listaPila[_cima];
+            }
+        }
+        public bool Push(int valor)
+        {
+            if (PilaLlena())
+            {
+                return false;
+            }
+            else
+            {
+                _cima++;
+                _listaPila[_cima] = valor;
+                return true;
+            }
+        
+        }
+        public int ExtraerElemento()
+        {
+            if (PilaVacia())
+            {
+                return _cima;
+            }
+            else
+            {
+                int aux = _listaPila[_cima];
+                _cima--;
+                return aux;
+            }
+        }
+        public bool LimpiarPila()
+        {
+            if (PilaVacia())
+            {
+                return false;
+            }
+            else
+            {
+                //while (!PilaVacia())
+                //{
+                //    ExtraerElemento();
+                //}
+                _cima = -1;
+                return true;
+            }
+        }
+
+        public void ImprimirPila()
+        { 
+            if (!PilaVacia())
+        for (int i=_cima,j=0; i>=0;i--,j++)
+                Console.WriteLine($"La pila {j+1} es { _listaPila[i]}");
+        else
+                Console.WriteLine("la pila esta vacia");
+        }
+
+    }
+}
---
+++ Pilas_Jerh/Program.cs
@@ -0,0 +1,53 @@
+﻿using Pilas_Jerh;
+
+Pila pila = new Pila();
+/* if (pila.PilaVacia())
+ {
+     Console.WriteLine("No hay nada pa' ");
+ }
+ else
+ {
+     Console.WriteLine("Si hay algo");
+ }*/
+if (!pila.PilaLlena())
+{
+    Console.WriteLine("Aun faltan espacios");
+}
+else
+{
+    Console.WriteLine("Esta llena");
+}
+Console.WriteLine($"La longitud de la pila es {pila.LongitudPila()}");
+Console.WriteLine($"La cantidad de elementos real es {pila.CantidadElemento()}");
+if (pila.ElementoCima() != -1)
+{
+    Console.WriteLine($"El valor del elemento cima es {pila.ElementoCima()}");
+}
+else
+{
+    Console.WriteLine("La pila no contiene elementos");
+}
+Console.WriteLine("Ingresar el valor");
+int valor = Convert.ToInt32(Console.ReadLine());
+if (pila.Push(valor))
+{
+    Console.WriteLine("El valor se inserto correctamente");
+}
+else
+{
+    Console.WriteLine("La pila se encuentra llena");
+}
+Console.WriteLine("---------------");
+pila.ImprimirPila();
+Console.WriteLine("---------------");
+//Console.WriteLine(pila.ExtraerElemento());
+int valor1 = pila.ExtraerElemento();
+if (valor1 != -1)
+{
+    Console.WriteLine($"El elemento extraido fue {valor1}");
+}
+else
+{
+    Console.WriteLine("La pila esta vacia");
+}
+
